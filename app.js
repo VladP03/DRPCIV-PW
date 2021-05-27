aa9c5329c402777e8f09eacd632641503ea274b1
Vlad Paraschiv <vladparaschiv03@gmail.com>
2021-05-27 09:41:33 +0300

    cookies & modificari

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const bodyParser = require('body-parser');
 const { Console } = require('console');
+const cookieParser=require('cookie-parser');
 
 const fs = require('fs');
 
@@ -20,29 +21,73 @@ app.use(bodyParser.json());
 // utilizarea unui algoritm de deep parsing care suportă obiecte în obiecte
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(cookieParser());
 
 // facem redirect catre pagina principala (home)
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/public/html/index.html');
+    res.redirect('/home');
 });
 
-app.get('/home.html', function(req, res) {
-    res.sendFile(__dirname + '/public/html/home.html');
+app.get('/home', function(req, res) {
+    res.render('home', {utilizator: req.cookies.utilizator});
 });
 
-app.get('/about.html', function(req, res) {
-    res.sendFile(__dirname + '/public/html/about.html');
+app.get('/about', function(req, res) {
+    res.render('about', {utilizator: req.cookies.utilizator});
 });
 
-app.get('/contact.html', function(req, res) {
-    res.sendFile(__dirname + '/public/html/contact.html');
+app.get('/contact', function(req, res) {
+    res.render('contact', {utilizator: req.cookies.utilizator});
 });
 
-app.get('/contact.html', function(req, res) {
-    res.sendFile(__dirname + '/public/html/contact.html');
+app.get('/login', function(req, res) {
+    res.clearCookie('utilizator');
+
+    res.render('login', {utilizator: req.cookies.utilizator});
+});
+
+app.get('/logout', function(req, res) {
+    res.clearCookie('utilizator');
+
+    res.redirect('/home');
+});
+
+app.post('/verificare-autentificare', (req,res) => {
+
+    console.log(req.body);
+
+	const fs = require('fs');	
+	fs.readFile('utilizatori.json', (err, data) => {
+		if (err) throw err;
+		let listaUseri = JSON.parse(data);
+
+		let ok =0;
+		for (let i=0;i<listaUseri.length;i++) {
+
+			if (listaUseri[i].username == req.body.user && listaUseri[i].password == req.body.pass) {
+				ok=1;
+
+				res.cookie("utilizator", listaUseri[i].username);
+
+                if (listaUseri[i].role == "admin") {
+                    res.sendFile(__dirname + '/public/html/adminHome.html');
+                } else {
+				    res.redirect('http://localhost:6789/');
+                }
+			}
+		}
+
+		if (ok == 0) {
+			res.cookie("utilizator", "mesajEroare");
+
+			res.redirect('http://localhost:6789/login');
+		}
+	})
 });
 
 app.get('/admin', (req,res) => {	
+
+    console.log(req.body);
 
     /*
 	if (req.session.role == "admin") {
--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,4 @@
-function schimbaContinut(resursa, jsFisier, jsFunctie) {
-    console.log("am intrat aici");
-
+function schimbaContinut(resursa) {
     var xmlhttp;
 
     if (window.XMLHttpRequest) {
@@ -8,27 +6,9 @@ function schimbaContinut(resursa, jsFisier, jsFunctie) {
         xmlhttp.onreadystatechange = function () {
             if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
                 document.getElementById("continut").innerHTML = xmlhttp.responseText;
-                
-                if (jsFisier) {
-                    var elementScript = document.createElement('script');
-                    elementScript.onload = function () {
-                        console.log("hello");
-                        if (jsFunctie) {
-                            window[jsFunctie]();
-                        }
-                    };
-                    elementScript.src = jsFisier;
-                    document.head.appendChild(elementScript);
-                } else {
-                    if (jsFunctie) {
-                        window[jsFunctie]();
-                    }
-                }
             }
         }
     }
-
-    console.log(xmlhttp.responseText);
 
     xmlhttp.open("GET", resursa + ".html", true);
     xmlhttp.send();
